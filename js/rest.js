9c8f107c57f2809f28fab727da1fc16f5683660a
Simon Furborg <kattassen@gmail.com>
2015-09-29 18:05:29 +0200

    Added "handling" of server down issue.

--- js/rest.js
+++ js/rest.js
@@ -1,5 +1,5 @@
 function saveRoute(name, json) {
-    alert("Send to server");
+    //alert("Send to server");
     $.ajax({
         type : "PUT",
         url: "http://127.0.0.1:8000/" + name,
@@ -7,13 +7,14 @@ function saveRoute(name, json) {
         data : "JKJKL",
         success : function(resp) {
             alert("success");
+        },
+        error: function (xhr, textStatus, errorThrown) {
+                alert("Server down?");
         }
     })
-
     .done(function( data ) {
         console.log("done");
     })
-
     .fail(function(xhr, textStatus, errorThrown) {
         //Prevent error on navigating from page
         if (xhr.status == 0 || xhr.readyState == 0)
