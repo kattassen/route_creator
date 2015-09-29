c1bf464a480ecdcfa6df319be00831d6c0140231
Simon Furborg <kattassen@gmail.com>
2015-09-29 14:13:08 +0200

    Added saveRoute function.

--- js/rest.js
+++ js/rest.js
@@ -1,9 +1,13 @@
-$(document).ready(function() {
+function saveRoute(name, json) {
+    alert("Send to server");
     $.ajax({
         type : "PUT",
-        url: "http://127.0.0.1:8000/TJO",
+        url: "http://127.0.0.1:8000/" + name,
         dataType : "text",
-        data : "HEJHEJ"
+        data : "JKJKL",
+        success : function(resp) {
+            alert("success");
+        }
     })
 
     .done(function( data ) {
