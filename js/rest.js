c87d24028d23c7132e8729d514b81ffec70de9eb
Simon Furborg <kattassen@gmail.com>
2015-09-30 08:30:27 +0200

    Removed ajax fail function in favour for error.

--- js/rest.js
+++ js/rest.js
@@ -1,11 +1,11 @@
 function saveRoute(name, json) {
     //alert("Send to server");
     $.ajax({
-        type : "PUT",
+        type : "POST",
         url: "http://127.0.0.1:8000/" + name,
         dataType : "text",
         data : "JKJKL",
-        success : function(resp) {
+        success : function (resp) {
             alert("success");
         },
         error: function (xhr, textStatus, errorThrown) {
@@ -21,16 +21,7 @@ function saveRoute(name, json) {
             }
         }
     })
-    .done(function( data ) {
+    .done(function (data) {
         console.log("done");
     })
-    .fail(function(xhr, textStatus, errorThrown) {
-        //Prevent error on navigating from page
-        if (xhr.status == 0 || xhr.readyState == 0)
-            return;
-        else
-        alert(xhr.status);
-        alert(textStatus);
-        alert(errorThrown);
-    });
 }
